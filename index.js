c7db8220e9d2db34d2fc662196b419dd8504004a
jeremias0711 <jgutierrezmo@unbosque.edu.co>
2026-05-31 16:39:33 -0500

    Menu contactanos 1

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const botonDesplegable = document.getElementById("desplegable");
 const menuDesplegable = document.querySelector(".menu-desplegable");
+
 const contactoBtn = document.getElementById("contacto-btn");
 const contactDropdown = document.querySelector(".contact-dropdown");
+
+if(contactoBtn && contactDropdown){
+
+    contactoBtn.addEventListener("click", function(e){
+
+        e.stopPropagation();
+
+        contactDropdown.classList.toggle("active");
+
+    });
+
+    document.addEventListener("click", function(e){
+
+        if(
+            !contactDropdown.contains(e.target) &&
+            !contactoBtn.contains(e.target)
+        ){
+
+            contactDropdown.classList.remove("active");
+
+        }
+
+    });
+
+}
 
 botonDesplegable.addEventListener("click", function() {
 
@@ -11,23 +37,3 @@ botonDesplegable.addEventListener("click", function() {
 
 });
 
-contactoBtn.addEventListener("click", function(e){
-
-    e.stopPropagation();
-
-    contactDropdown.classList.toggle("active");
-
-});
-
-document.addEventListener("click", function(e){
-
-    if(
-        !contactDropdown.contains(e.target) &&
-        !contactoBtn.contains(e.target)
-    ){
-
-        contactDropdown.classList.remove("active");
-
-    }
-
-});
